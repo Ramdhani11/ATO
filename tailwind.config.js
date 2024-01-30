709522f7caca09e85d049629ce96199032daf93a
Ramdhani11 <anwarramdhanie@gmail.com>
2024-01-30 14:02:25 +0700

    -

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,7 @@ export default {
       },
       backgroundImage: {
         benner:
-          "linear-gradient(to bottom,#C2DBFF4D,#C2DBFF4D),url('/benner.jpeg')",
+          "linear-gradient(to bottom,#C2DBFF4D,#C2DBFF4D),url('./benner.jpeg')",
       },
     },
     screens: {
