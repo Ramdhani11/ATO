de9a7b77bd054478d44981dfd7cd4c07468ec512
Ramdhani11 <anwarramdhanie@gmail.com>
2024-02-02 11:37:32 +0700

    -

--- src/components/ListCard.tsx
+++ src/components/ListCard.tsx
@@ -6,7 +6,7 @@ const ListCard = () => {
 
   return (
     <div
-      className={`cursor-pointer  w-[412px] px-[8px] flex gap-4 items-centerpy-4  ${
+      className={`cursor-pointer  w-[412px] px-[8px] flex gap-4 items-center py-4  ${
         checkTheme
           ? "bg-[#151d29] hover:bg-[#4c6385] "
           : "bg-white hover:bg-secondary"
